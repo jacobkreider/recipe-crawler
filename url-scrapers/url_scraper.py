79d2d5a9a687bdadf15d4f7bf04d66227924f0de
jacobkreider <jacob@happinessdata.com>
2019-08-14 16:26:05 -0400

    Created dict for recipe info

--- url-scrapers/url_scraper.py
+++ url-scrapers/url_scraper.py
@@ -1,10 +1,13 @@
 ''' Uses Beautiful Soup to scrape all top recipes URLs from our chosen sites'''
 
-import top_recipes_urls
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from recipe_scrapers import scrape_me
 
-html = urlopen("https://www.allrecipes.com/recipes/16492/everyday-cooking/special-collections/allrecipes-allstars/") # Insert your URL to extract
+
+
+html = urlopen("https://www.allrecipes.com/recipes/16492/everyday-cooking/special-collections/allrecipes-allstars/")
 bsObj = BeautifulSoup(html.read())
 
 allrecipes_urls = []
@@ -15,5 +18,27 @@ for link in bsObj.find_all('a'):
 
 allrecipes_urls = [s for s in allrecipes_urls[3:] if 'https://www.allrecipes.com/recipe/' in s]
 
+recipe_dict = {}
+used_recipes = []
 
 print(allrecipes_urls)
+
+for recipe in allrecipes_urls:
+    if recipe in used_recipes:
+        pass
+    else:
+        scraper = scrape_me(recipe)
+        recipe_info = {'title': scraper.title(), 'yields': scraper.yields(), 'ingredients': scraper.ingredients()
+                       , 'instructions': scraper.instructions()}
+        recipe_dict[scraper.title()] = recipe_info
+        used_recipes.append(recipe)
+
+        #recipe_dict[scraper.title()]['total_time'] = scraper.total_time()
+        #recipe_dict[scraper.title()]['yields'] = scraper.yields()
+        #recipe_dict[scraper.title()]['ingredients'] = scraper.ingredients()
+        #recipe_dict[scraper.title()]['instructions'] = scraper.instructions()
+        #recipe_dict[scraper.title()]['links'] = scraper.links()
+
+
+print(recipe_dict)
+
